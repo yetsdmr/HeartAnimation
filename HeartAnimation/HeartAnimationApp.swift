d981f29098cf720a71a29b39406ee655683ab148
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-24 19:40:33 +0300

    Initial Commit

---
+++ HeartAnimation/HeartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartAnimationApp.swift
+//  HeartAnimation
+//
+//  Created by Yunus Emre Taşdemir on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
